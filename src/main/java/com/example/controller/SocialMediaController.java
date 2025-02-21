c5efa94f90d54c935304b09fece1edfccd6dca9e
mlsloynaz <mlsloynaz@yahoo.com>
2025-02-21 20:15:01 +0000

    User registration

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,16 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.entity.Account;
+import com.example.exception.AlreadyExistsException;
+import com.example.service.AccountService;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +18,26 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
-public class SocialMediaController {
+ @RestController
+ public class SocialMediaController {
+    private AccountService accountService;
+
+    @Autowired
+    public SocialMediaController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<?> registerAccount(@RequestBody  Account account){
+        try{
+            Account registeredAccount = accountService.registerAccount(account);                                          
+            return ResponseEntity.ok(registeredAccount);
+        }catch(AlreadyExistsException ex){
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }catch(RuntimeException ex){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
 
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,40 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.exception.AlreadyExistsException;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService(AccountRepository accountRepository) {
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount(Account account) throws AlreadyExistsException, IllegalArgumentException {
+        boolean accountExists = accountRepository.existsByUsername(account.getUsername());
+
+        if (accountExists) {
+            throw new AlreadyExistsException();
+        }
+        if (!validateAccountData(account)) {
+            throw new IllegalArgumentException("Client error");
+        }
+
+        return accountRepository.save(account);
+    }
+
+    private boolean validateAccountData(Account account) {
+        String username = account.getUsername();
+        String password = account.getPassword();
+        if (username.length() == 0 || password.length() < 5) {
+            return false;
+        }
+        return true;
+    }
 }
